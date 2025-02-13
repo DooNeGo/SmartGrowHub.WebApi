8adfe406587412dadcf6b093df8b20dcd60ef994
Mathew Kastrama <matveyprostomac@gmail.com>
2025-02-13 14:34:03 +0300

    Simplify auth tokens DTOs

--- SmartGrowHub.WebApi/Modules/Extensions/AuthTokensExtensions.cs
+++ SmartGrowHub.WebApi/Modules/Extensions/AuthTokensExtensions.cs
@@ -6,8 +6,5 @@ namespace SmartGrowHub.WebApi.Modules.Extensions;
 public static class AuthTokensExtensions
 {
     public static AuthTokensDto ToDto(this AuthTokens tokens) =>
-        new(tokens.AccessToken.To(), ToDto(tokens.RefreshToken));
-
-    public static RefreshTokenDto ToDto(this RefreshToken token) =>
-        new(token.Ulid.ToString(), token.Expires);
+        new(tokens.AccessToken.To(), tokens.RefreshToken.Ulid.ToString());
 }
